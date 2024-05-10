a07a0b8f692aef0a3e0d6af68f935892936b926a
Paul <mothcar@gmail.com>
2024-05-10 20:03:18 +0900

    justify-content

--- src/App.jsx
+++ src/App.jsx
@@ -8,8 +8,8 @@ function App() {
         <div className="box" id="box1">1</div>
         <div className="box" id="box2">2</div>
         <div className="box" id="box3">3</div>
-        <div className="box" id="box4">4</div>
-        <div className="box" id="box5">5</div>
+        {/* <div className="box" id="box4">4</div>
+        <div className="box" id="box5">5</div> */}
       </div>
     </>
   )
