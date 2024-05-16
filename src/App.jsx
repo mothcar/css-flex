96e5090314aa918b6e5ae285c0fd68f7e3d2b5aa
Paul <mothcar@gmail.com>
2024-05-16 16:55:08 +0900

    tailwind

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,30 @@
-
 function App() {
-
   return (
     <>
       <div>App</div>
+      <div>
+        <h1 className="text-3xl font-bold ">Hello world!</h1>
+      </div>
       <div className="container">
-        <div className="box" id="box1">1</div>
-        <div className="box" id="box2">2</div>
-        <div className="box" id="box3">3</div>
+        <div className="box bg-slate-600" id="box1">
+          1
+        </div>
+        <div className="box" id="box2">
+          2
+        </div>
+        <div className="box" id="box3">
+          3
+        </div>
         {/* <div className="box" id="box4">4</div>
         <div className="box" id="box5">5</div> */}
       </div>
+
+      <div>
+        <div className="min-h-[100px] rounded-lg shadow bg-orange-400"></div>
+        <div className="min-h-[100px] rounded-lg shadow bg-purple-400"></div>
+      </div>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
